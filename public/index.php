27bf7584acd896c1eb8e0e4f85a424dff0ba913e
Sergi <sergicoca32@gmial.com>
2025-10-17 16:39:11 +0200

    Afegit index.php bàsic (Hola Mön)

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>MiWeb</title>
+</head>
+<body>
+    <h1>Hola Mön!</h1>
+    <p>Aquesta és la meva web Docker i GitHub</p>
+</body>
+</html>
