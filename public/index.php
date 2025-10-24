d057dcf328fa459b5fc3f5077b451b783118cc84
Sergi Coca <adaw24scoca@inslaferreria.cat>
2025-10-24 11:30:21 +0200

    Pas 9: Afegit test connexió BD a index.php

--- public/index.php
+++ public/index.php
@@ -2,10 +2,26 @@
 <html lang="ca">
 <head>
     <meta charset="UTF-8">
-    <title>MiWeb</title>
+    <title>MiWeb amb Test BD</title>
 </head>
 <body>
     <h1>Hola Mön!</h1>
-    <p>Aquesta és la meva web Docker i GitHub</p>
+    <p>Web desplegada amb Docker i GitHub Actions.</p>
+
+    <?php
+    // Test connexió BD (credencials del docker-compose.yml)
+    $host = "db";       // Host intern de Docker
+    $username = "user"; // Usuari
+    $password = "password"; // Contrasenya
+    $dbname = "demo";   // Nom BD
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "<p>Connexió a BD </p>";
+    } catch (PDOException $e) {
+        echo "<p> Error connexió BD</p>";
+    }
+    ?>
 </body>
 </html>
